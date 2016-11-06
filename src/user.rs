16e018b8404f347682eca4bfd7cd741b6b09408a
Antonin Carette <antonin.carette@gmail.com>
2016-11-06 18:30:52 +0100

    The get function returns a Result type, composed by a structure or an error

--- src/user.rs
+++ src/user.rs
@@ -1,5 +1,8 @@
 use client::GithubClient;
+
 use hyper::method::Method;
+
+use serde_json;
 
 /// URL to access the Github API for User
 const USER_API: &'static str = "users";
@@ -10,7 +13,7 @@ pub struct UserClient<'a> {
     /// The Github client
     github_client: &'a GithubClient,
     /// The username to build requests
-    username: String,
+    pub username: String,
 }
 
 impl<'a> UserClient<'a> {
@@ -32,36 +35,93 @@ impl<'a> UserClient<'a> {
     }
 
     /// Returns a String that represents the body of the request for a user, or an error
-    pub fn get(&self) -> Result<String, String> {
-        self.github_client.process_request(Method::Get, &format!("{}/{}", USER_API, self.username))
+    pub fn get(&self) -> Result<UserInfoStructure, String> {
+        match self.github_client
+            .process_request(Method::Get, &format!("{}/{}", USER_API, self.username)) {
+            Ok(response) => {
+                match serde_json::from_str(&response) {
+                    Ok(user_structure) => Ok(user_structure),
+                    Err(error) => {
+                        Err(format!("Error when converting the string request from Github to \
+                                     JSON: {}, for response {}",
+                                    error,
+                                    response))
+                    }
+                }
+            }
+            Err(error) => Err(error),
+        }
     }
 }
 
 /// Interesting fields that represent globally a User on Github
-#[derive(Serialize, Deserialize)]
-struct UserStructure {
-    login: String,
-    id: usize,
-    avatar_url: String,
-    gravatar_id: String,
-    url: String,
-    html_url: String,
-    followers_url: String,
-    following_url: String,
-    gists_url: String,
-    starred_url: String,
-    subscriptions_url: String,
-    organizations_url: String,
-    repos_url: String,
-    site_admin: bool,
-    name: String,
-    company: String,
-    location: String,
-    email: String,
-    hireable: bool,
-    bio: String,
-    public_repos: usize,
-    public_gists: usize,
-    followers: usize,
-    following: usize,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserInfoStructure {
+    #[serde(skip_serializing_if="Option::is_none")]
+    login: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    id: Option<usize>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    avatar_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    gravatar_id: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    html_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    followers_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    following_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    gists_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    starred_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    subscriptions_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    organizations_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    repos_url: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    site_admin: Option<bool>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    name: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    email: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    public_repos: Option<usize>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    public_gists: Option<usize>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    followers: Option<usize>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    following: Option<usize>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    company: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    location: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    hireable: Option<bool>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    bio: Option<String>,
+}
+
+/// Fields that can be modified using the Github API, for a given user
+#[derive(Debug, Serialize)]
+pub struct UserUpdateStructure<'a> {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    email: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    blog: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    company: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    location: Option<&'a str>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    hireable: Option<bool>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    bio: Option<&'a str>,
 }
