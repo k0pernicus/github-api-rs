5343c6d2b22943be211ffaee22b93a5c2ebcb087
k0pernicus <antonin.carette@gmail.com>
2016-11-07 23:05:48 +0100

    Add new tests

--- src/lib.rs
+++ src/lib.rs
@@ -16,9 +16,9 @@ header! { (XRateLimitRemaining, "X-RateLimit-Remaining") => [usize] }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use client::GithubClient;
-    use user::UserClient;
+    use std::env;
+    use user::UserUpdateStructure;
 
     const DEFAULT_API_KEY: &'static str = "HelloWorld12345";
 
@@ -31,13 +31,45 @@ mod tests {
 
     #[test]
     fn test_user() {
-        let github_client = GithubClient::new("myself", DEFAULT_API_KEY);
-        // let myself_client = github_client.get_myself_client();
-        let another_user_client = github_client.get_user_client("k0pernicus");
-        // assert!(myself_client.username == another_user_client.username);
-        match another_user_client.get() {
-            Ok(value) => println!("{:?}", value),
-            Err(error) => println!("{:?}", error),
+        let api_key = "GITHUB_API_RS";
+        let GITHUB_API_KEY = match env::var(api_key) {
+            Ok(val) => val,
+            Err(e) => DEFAULT_API_KEY.to_string(),
+        };
+        let github_client = GithubClient::new("k0pernicus", &GITHUB_API_KEY);
+        let myself = github_client.get_myself_client();
+        match myself.get() {
+            Ok(value) => println!("SUCCESS: {:?}", value),
+            Err(error) => println!("ERROR: {:?}", error),
         }
     }
+
+    // #[test]
+    // fn modify_user() {
+    //     let key = "GITHUB_API_RS";
+    //     let GITHUB_API_KEY = match env::var(key) {
+    //         Ok(val) => val,
+    //         Err(e) => DEFAULT_API_KEY.to_string(),
+    //     };
+    //     let github_client = GithubClient::new("k0pernicus", &GITHUB_API_KEY);
+    //     let myself_client = github_client.get_myself_client();
+    //     match myself_client.get() {
+    //         Ok(value) => println!("GET SUCCESS: {:?}", value),
+    //         Err(error) => println!("GET ERROR: {:?}", error),
+    //     }
+    //     let new_infos = UserUpdateStructure {
+    //         name: None,
+    //         email: None,
+    //         blog: None,
+    //         company: Some(String::from("DernierCri")),
+    //         location: Some(String::from("Lille, France")),
+    //         hireable: Some(false),
+    //         bio: None,
+    //     };
+    //     match myself_client.update(&new_infos) {
+    //         Ok(value) => println!("UPDATE SUCCESS: {:?}", value),
+    //         Err(error) => println!("UPDATE ERROR: {:?}", error),
+    //     }
+    // }
+
 }
